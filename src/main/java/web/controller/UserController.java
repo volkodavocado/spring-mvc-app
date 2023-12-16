c3916929e8ca8c97dc2ffe70c2e5f243aa4877ba
volkodavocado <olga.braulova@gmail.com>
2023-12-16 13:16:38 +0300

    update

--- src/main/java/web/controller/UserController.java
+++ src/main/java/web/controller/UserController.java
@@ -3,14 +3,12 @@ package web.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 import web.model.User;
 import web.service.UserService;
 
 @Controller
+@RequestMapping("/spring-mvc-app")
 public class UserController {
     private final UserService service;
 
@@ -19,38 +17,38 @@ public class UserController {
         this.service = service;
     }
 
-    @GetMapping("/")
+    @GetMapping
     public String printAll(Model model) {
         model.addAttribute("users", service.getAll());
-        return "index";
+        return "spring-mvc-app/index";
     }
 
     @GetMapping("/new")
     public String printAddForm(@ModelAttribute("user") User user) {
-        return "new";
+        return "spring-mvc-app/new";
     }
 
     @PostMapping("/new")
     public String add(@ModelAttribute("user") User user) {
         service.save(user);
-        return "redirect:/";
+        return "redirect:/spring-mvc-app";
     }
 
     @PostMapping("/delete")
     public String remove(@RequestParam("id") long id) {
         service.removeById(id);
-        return "redirect:/";
+        return "redirect:/spring-mvc-app";
     }
 
     @GetMapping("/edit")
     public String printEditForm(@RequestParam("id") long id, Model model) {
         model.addAttribute("user", service.getById(id));
-        return "edit";
+        return "spring-mvc-app/edit";
     }
 
     @PostMapping("/edit")
     public String edit(@ModelAttribute("user") User user, @RequestParam("id") long id) {
         service.update(user, id);
-        return "redirect:/";
+        return "redirect:/spring-mvc-app";
     }
 }
